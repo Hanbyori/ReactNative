c4bf446e9499b4ba4d6ef9f48be067ad5a373cb0
한 별 <sinsarigger@gmail.com>
2021-01-23 16:34:26 +0000

    1.0.1

--- Project/App.js
+++ Project/App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style = {styles.container}>
+            <Text style = {style.message}>Open up App.js to start working on your app!</Text>
+			<Image source = {require('')} style = {style.pictrue}></Image>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+	message: {
+		color: red
+	},
+	pictrue: {
+		witdh: 100,
+		height: 100
+	},
 });
