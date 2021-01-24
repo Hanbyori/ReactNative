988e7567e67cf170b03f0b13184c5b6dd04cf133
한 별 <sinsarigger@gmail.com>
2021-01-24 00:06:51 +0000

    1.0.2

--- Project/App.js
+++ Project/App.js
@@ -5,20 +5,20 @@ export default function App() {
     return (
         <View style = {styles.container}>
             <Text style = {style.message}>Open up App.js to start working on your app!</Text>
-			<Image source = {require('')} style = {style.pictrue}></Image>
+			<Image source = {require('./assets/unnamed.jpg')} style = {style.pictrue}></Image>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+	container: {
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
     },
 	message: {
-		color: red
+		color: 'red'
 	},
 	pictrue: {
 		witdh: 100,
